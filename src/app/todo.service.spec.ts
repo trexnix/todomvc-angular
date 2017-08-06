c9c10c1ca62653e784454ce6d01c51c68afe4fab
Trexnix <trexnix@gmail.com>
2017-08-07 02:29:34 +0700

    Add TodoService service

---
+++ src/app/todo.service.spec.ts
@@ -0,0 +1,70 @@
+import { TestBed, inject } from '@angular/core/testing';
+
+import { TodoService } from './todo.service';
+
+describe('TodoService', () => {
+  let service: TodoService;
+
+  beforeEach(() => {
+    localStorage.clear();
+    service = new TodoService;
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should have no todos by default', () => {
+    expect(service.getAll()).toEqual([]);
+  });
+
+  it('should create new todo', () => {
+    service.create('new todo');
+    expect(service.getAll().length).toEqual(1);
+    expect(service.getAll()[0].title).toEqual('new todo');
+  });
+
+  it('should list latest todos first', (done: DoneFn) => {
+    service.create('first todo');
+    setTimeout(() => {
+      service.create('second todo');
+      expect(service.getAll().length).toEqual(2);
+      expect(service.getAll()[0].title).toEqual('second todo');
+      done();
+    }, 1000)
+  });
+
+  it('#getActive should list active todos only', () => {
+    service.create('first todo');
+    service.create('second todo');
+    service.toggleDone(service.getAll()[0]);
+    expect(service.getActive().length).toEqual(1);
+    expect(service.getActive()[0].done).toBeFalsy();
+  });
+
+  it('#getCompleted should list completed todos only', () => {
+    service.create('first todo');
+    service.create('second todo');
+    service.toggleDone(service.getAll()[0]);
+    expect(service.getCompleted().length).toEqual(1);
+    expect(service.getCompleted()[0].done).toBeTruthy();
+  });
+
+  it('should update a todo', () => {
+    service.create('new todo');
+    const todo = service.getAll()[0];
+    expect(todo.done).toBeFalsy();
+    service.toggleDone(todo);
+    expect(todo.done).toBeTruthy();
+
+    service.updateTitle(todo, 'todo with new title');
+    expect(todo.title).toEqual('todo with new title');
+  });
+
+  it('should remove a todo', () => {
+    service.create('new todo');
+    const todo = service.getAll()[0];
+    service.destroy(todo);
+    expect(service.getAll().length).toEqual(0);
+  });
+});
---
+++ src/app/todo.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+
+import { Todo } from './todo';
+
+@Injectable()
+export class TodoService {
+  LOCALSTORAGE_KEY = 'todomvc_todos';
+  private todos: Array<Todo>;
+  source = new Subject<void>();
+
+  constructor() {
+    this.todos = this.loadData();
+    this.source.subscribe(() => this.saveData());
+  }
+
+  private loadData() {
+    return JSON.parse(localStorage.getItem(this.LOCALSTORAGE_KEY) || '[]');
+  }
+
+  private saveData() {
+    localStorage.setItem(this.LOCALSTORAGE_KEY, JSON.stringify(this.todos));
+  }
+
+  getAll(): Array<Todo> {
+    return this.todos.sort((a, b) => b.createdAt - a.createdAt);
+  }
+
+  getActive(): Array<Todo> {
+    return this.getAll().filter(todo => !todo.done);
+  }
+
+  getCompleted(): Array<Todo> {
+    return this.getAll().filter(todo => todo.done);
+  }
+
+  create(title: string) {
+    this.todos.push(new Todo(title));
+    this.source.next();
+  }
+
+  toggleDone(todo: Todo) {
+    todo.done = !todo.done;
+    this.source.next();
+  }
+
+  updateTitle(todo: Todo, text: string) {
+    todo.title = text;
+    this.source.next();
+  }
+
+  destroy(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+    if (index > -1) this.todos.splice(index, 1);
+    this.source.next();
+  }
+}
